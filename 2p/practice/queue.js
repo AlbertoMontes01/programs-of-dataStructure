8614dadabbacbdf331d1430174214feb8ca66352
Alberto Montes <61394785+AlbertoMontes01@users.noreply.github.com>
2022-10-14 18:46:19 +0000

    commit again

--- 2p/practice/queue.js
+++ 2p/practice/queue.js
@@ -51,12 +51,12 @@ function Queue() {
     }
   }
 }
+/*
 //
 // test program
 //
 
 let q = new Queue()
-//console.log(q.numrandom())
 q.enqueue( 1 + Math.floor(Math.random() * n))
 q.enqueue( 1 + Math.floor(Math.random() * n))
 q.enqueue( 1 + Math.floor(Math.random() * n))
@@ -80,3 +80,48 @@ console.log("Back of queue: " + q.back());
 q.dequeue()
 console.log(q.toString())
 console.log('done')
+*/
+function distribute(nums, queues, n, digit) { // digit represents either the 1s
+// or 10s place
+for (var i = 0; i < n; ++i) {
+if (digit == 1) {
+queues[nums[i]%10].enqueue(nums[i]);
+}
+else {
+queues[Math.floor(nums[i] / 10)].enqueue(nums[i]);
+}
+}
+}
+
+function collect(queues, nums) {
+var i = 0;
+for (var digit = 0; digit < 10; ++digit) {
+while (!queues[digit].empty()) {
+  nums[i++] = queues[digit].dequeue();
+}
+}
+}
+
+function dispArray(arr) {
+for (var i = 0; i < arr.length; ++i) {
+putstr(arr[i] + " ");
+}
+}
+
+var queues = [];
+for (var i = 0; i < 10; ++i) {
+queues[i] = new Queue();
+}
+var nums = [];
+for (var i = 0; i < 10; ++i) {
+nums[i] = Math.floor(Math.floor(Math.random() * 101));
+}
+console.log("Before radix sort: ");
+dispArray(nums);
+distribute(nums, queues, 10, 1);
+collect(queues, nums);
+distribute(nums, queues, 10, 10);
+collect(queues, nums);
+
+console.log("\n\nAfter radix sort: ");
+dispArray(nums);
---
+++ 3p/1linkedlist/4/useNode.js
@@ -0,0 +1,8 @@
+//
+//
+//
+import Node from '../../../advm/linked/node.js'
+
+let n=new Node('b')
+
+console.log(n)
